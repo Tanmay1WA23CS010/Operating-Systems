af137a6bbf2aa9f72d3f6da3995c367187e0b0fd
Tanmay1WA23CS010 <153522911+Tanmay1WA23CS010@users.noreply.github.com>
2025-05-22 12:03:26 +0530

    Update allinoneee.c

--- os preactise/Lab8(fcfs,LRUOptimal)/allinoneee.c
+++ os preactise/Lab8(fcfs,LRUOptimal)/allinoneee.c
@@ -1,115 +1,146 @@
 #include <stdio.h>
-//page replacement algo 
-void FIFO(char pages[], int n, int frames){
-    
-    int page_faults = 0;
-    int hit =0;
-    int mem[frames];
-    int front = 0;
-    int rear = 0;
-    for(int i =0; i< frames; i++){
-        mem[i] = -1;
-    }
-    for(int i =0; i< n;i++){
-        int page = pages[i]-'0';
+
+void FIFO(char pages[], int n, int frames) {
+    int mem[frames], front = 0, page_faults = 0, hit = 0;
+    for (int i = 0; i < frames; i++) mem[i] = -1;
+
+    for (int i = 0; i < n; i++) {
+        int page = pages[i] - '0';
         int found = 0;
-        for(int j = 0; j<frames; j++){
-            if(mem[j] == page){
+
+        for (int j = 0; j < frames; j++) {
+            if (mem[j] == page) {
                 hit++;
                 found = 1;
                 break;
             }
         }
-        if(found == 0){
+
+        if (!found) {
+            mem[front] = page;
+            front = (front + 1) % frames;
             page_faults++;
-            mem[front] = pages[i];
-            front = (front +1) % frames;
-
         }
-
-
     }
+
+    printf("FIFO:\nPage Faults: %d\nHits: %d\n", page_faults, hit);
 }
 
-void LRU(char pages[], int n ,int frames){
+void LRU(char pages[], int n, int frames) {
+    int mem[frames], lastused[frames], page_faults = 0, hit = 0;
 
-    int page_faults = 0;
-    int hit =0;
-    int mem[frames], lastused[frames];
-   
-    for(int i =0; i< frames; i++){
+    for (int i = 0; i < frames; i++) {
         mem[i] = -1;
+        lastused[i] = -1;
     }
 
-    for(int i=0; i< n; i++){
-        int page = pages[i]-'0';
- 
+    for (int i = 0; i < n; i++) {
+        int page = pages[i] - '0';
         int found = 0;
-        for(int j = 0; j< frames ;j++){
-            if(mem[j] == pages[i]-'0'){
+
+        for (int j = 0; j < frames; j++) {
+            if (mem[j] == page) {
                 hit++;
-                lastused[j] =i;
+                lastused[j] = i;
                 found = 1;
+                break;
+            }
         }
-        if(found == 0){
+
+        if (!found) {
             page_faults++;
-            int index =-1; int lru = 999;
-            for(int k=0; k< frames; k++){
-                if(mem[j] == -1){
-                    index =j;
+            int index = -1;
+
+            for (int j = 0; j < frames; j++) {
+                if (mem[j] == -1) {
+                    index = j;
                     break;
                 }
-                if(lastused[k]< lru){
-                    lru = lastused[k];
-                    index = j;
+            }
+
+            if (index == -1) {
+                int lru = i;
+                for (int j = 0; j < frames; j++) {
+                    if (lastused[j] < lru) {
+                        lru = lastused[j];
+                        index = j;
+                    }
                 }
             }
+
             mem[index] = page;
             lastused[index] = i;
         }
     }
+
+    printf("LRU:\nPage Faults: %d\nHits: %d\n", page_faults, hit);
 }
-}
 
-void OPTIMAL(char pages[], int n ,int frames){
+void OPTIMAL(char pages[], int n, int frames) {
+    int mem[frames], page_faults = 0, hit = 0;
 
-    int page_faults = 0;
-    int hit =0;
-    int mem[frames];
+    for (int i = 0; i < frames; i++) mem[i] = -1;
 
-    for(int i =0; i< frames; i++){
-        mem[i] = -1;
-    }
-
-    for(int i=0; i< n; i++){
-        int page = pages[i]-'0';
-     
+    for (int i = 0; i < n; i++) {
+        int page = pages[i] - '0';
         int found = 0;
-        for(int j = 0; j< frames ;j++){
-            if(mem[j] == pages[i]-'0'){
+
+        for (int j = 0; j < frames; j++) {
+            if (mem[j] == page) {
                 hit++;
                 found = 1;
+                break;
+            }
         }
-        if(found == 0){
+
+        if (!found) {
             page_faults++;
-            int index =-1; int farthest = -1;
-            for(int k=0; k< frames; k++){
-                if(mem[j] == -1){
-                    index =j;
+            int index = -1;
+
+            for (int j = 0; j < frames; j++) {
+                if (mem[j] == -1) {
+                    index = j;
                     break;
                 }
-                for(int i= 0; i< n;i++){
-                    if(mem[k] == page){
-                        if(i>farthest){
-                            farthest = i;
-                            index = k;
+            }
+
+            if (index == -1) {
+                int farthest = -1, pos = -1;
+                for (int j = 0; j < frames; j++) {
+                    int next_use = -1;
+                    for (int k = i + 1; k < n; k++) {
+                        if (pages[k] - '0' == mem[j]) {
+                            next_use = k;
+                            break;
                         }
                     }
+                    if (next_use == -1) {
+                        pos = j;
+                        break;
+                    }
+                    if (next_use > farthest) {
+                        farthest = next_use;
+                        pos = j;
+                    }
                 }
+                index = pos;
             }
+
             mem[index] = page;
-            
         }
     }
+
+    printf("OPTIMAL:\nPage Faults: %d\nHits: %d\n", page_faults, hit);
 }
+
+int main() {
+    char pages[] = {'1', '2', '3', '4', '1', '2', '5', '1', '2', '3', '4', '5'};
+    int n = sizeof(pages) / sizeof(pages[0]);
+    int frames = 3;
+
+    FIFO(pages, n, frames);
+    LRU(pages, n, frames);
+    OPTIMAL(pages, n, frames);
+
+    return 0;
 }
